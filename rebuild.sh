e9afbaffab0e3c44b721fd3526383f7f4797c87a
Chris Allen <cma@bitemyapp.com>
2014-10-18 03:49:27 -0500

    credit

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# https://twitter.com/mwotton made this. Send him love.
+
 inotifywait -m -r . -e CLOSE_WRITE | grep --line-buffered "\.md$" | grep -v --line-buffered flymake | collapse | while read x; do make && printf "\n *** Build succeeded! *** \n"; done
